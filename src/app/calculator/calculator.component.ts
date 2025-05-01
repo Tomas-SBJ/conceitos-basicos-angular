2db3b029668d71a18cf94063b6d35f919cb53616
Tomas Schaefer Borges de Jesus <tomassbj@gmail.com>
2025-05-01 10:39:41 -0300

    feat: include operator to calculator

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-calculator',
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './calculator.component.html',
   styleUrl: './calculator.component.scss'
 })
@@ -11,9 +12,34 @@ export class CalculatorComponent {
   numberOne: number = 0;
   numberTwo: number = 0;
   result: number = 0;
+  operation: string = "";
+  error: string = "";
 
   resultCalculator() {
-    console.log("Chamando método");
-    this.result = this.numberOne + this.numberTwo;
+    switch (this.operation) {
+      case "sum":
+        this.result = this.numberOne + this.numberTwo;
+        break;
+
+      case "subtract":
+        this.result = this.numberOne - this.numberTwo;
+        break;
+        
+      case "multiply":
+        this.result = this.numberOne * this.numberTwo;
+        break;
+
+      //TODO: Verificar como resetar o erro;
+      case "split": 
+        if (this.numberTwo == 0) {
+          this.error = "Valor inválido para operação!";
+          break;
+        }
+        this.result = this.numberOne / this.numberTwo;
+        break;
+
+      default:
+        break;
+    }
   }
 }
